ecec81a4898865a8b52e820feba8d7b5669a2e74
Your Name <thomas.w.yee@gmail.com>
2025-05-26 21:29:50 -0400

    ADD: create a MedalTableRow component and setup basic folder structure for the project

---
+++ src/app/components/MedalTableRow/MedalTableRow.tsx
@@ -0,0 +1,46 @@
+import { getFlagBackgroundPosition } from '@utils/flagUtil';
+import styles from './MedalTableRow.module.css';
+
+export type Country = {
+  code: string;
+  gold: number;
+  silver: number;
+  bronze: number;
+  total?: number;
+  rank?: number;
+};
+
+export default function MedalTableRow({
+  code,
+  gold,
+  silver,
+  bronze,
+  total,
+  rank,
+}: Country) {
+  const backgroundPosition = getFlagBackgroundPosition(code);
+
+  return (
+    <tr className={styles.row}>
+      <td className={styles.rankCell}>
+        <p>{rank}</p>
+        <span
+          className={styles.flag}
+          style={{
+            backgroundPosition,
+            ...(code === 'SUI' && { backgroundPositionX: '-5px' }),
+            width: code === 'SUI' ? '18px' : '28px',
+            height: '17px',
+            backgroundImage: "url('/flags.png')",
+            display: 'inline-block',
+          }}
+        />
+        <p>{code}</p>
+      </td>
+      <td className={styles.cell}>{gold}</td>
+      <td className={styles.cell}>{silver}</td>
+      <td className={styles.cell}>{bronze}</td>
+      <td className={styles.cell}>{total}</td>
+    </tr>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Medal Count App',
+  description: 'A simple app to track medal counts',
+};
 
 export default function RootLayout({
   children,
